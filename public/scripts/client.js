d9a77d42605c16c469dadfeaec327624c4e48af2
USBrido <obrido@gmail.com>
2019-12-18 23:46:22 +0000

    First commit, forgot to do it because I was having a blast

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+  $('#form').click(function(event) {
+    event.preventDefault();
+    loadTweets();
+    if ($('#textbox').val() === null || $('#textbox').val() === '') {
+      $("#alert1").slideDown(200);
+    } else {
+      $("#alert1").slideUp(200);
+
+      $.ajax('/tweets', {method: 'POST', data: $(this).serialize()})
+      .then(function (tweetData) {
+        console.log('SSomething else:', tweetData);
+        renderTweet(tweetData);
+        $('#textbox').val('');
+
+      });
+    }
+
+ 
+
+  });
+
+  const escape = (str) => {
+    const div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  function renderTweet(tweetdata) {
+    for (let item = 0; item < tweetdata.length; item++) {
+      const escape = (str) => {
+  const div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+      let userTweet = `
+      <section id="tweet-container">
+        <article class="oldtweet">
+          <header class="tweethead">
+            <img src="${escape(tweetdata[item].user.avatars)}"/> 
+            <p class="name">${escape(tweetdata[item].user.name)}</p>
+            <p class="handle">${escape(tweetdata[item].user.handle)}</p>
+          </header>
+        <div class="tweetbody">
+          <p>${escape(tweetdata[item].content.text)} </p>            
+        </div>
+        <footer class="tweetfoot">
+        <p class=timecounter>hgjhgcjgfcjg</p>
+        </footer>
+        </article>
+      </section>`;
+      $('#tweet-container').prepend(userTweet);
+    }
+  }
+
+  function loadTweets() {
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function (tweetData) {
+      // console.log('Success: ', tweetData);
+      renderTweet(tweetData);
+    });
+}
+loadTweets();
+});
+
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+$("#textbox").on('keyup', function(value) {
+  let charactersTyped = $(this).val().length
+  let charactersLeft = 140 - charactersTyped ;
+  $(this).siblings('.counter').text(charactersLeft)
+  if (charactersLeft <= 0) {
+    $("#alert2").slideDown(200);
+    $(this).siblings('.counter').css({'color': 'red'});
+  }else{
+    $("#alert2").slideUp(200);
+    $(this).siblings('.counter').css({'color': 'black'});
+  }
+  
+  });
+
+});
+
